5afc44d320d8b21d9fd020d466f50df9eae360de
Malleshkumar G R <malleshkumargr.code@gmail.com>
2025-05-27 18:06:31 +0530

    Add a delete button

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -17,8 +17,8 @@ import { Theme } from "@radix-ui/themes";
 // });
 
 const inter = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -33,12 +33,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable}`}
-      >
+      <body className={`${inter.variable}`}>
         <Theme accentColor="violet">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
